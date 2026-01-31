6854d15db6da71232b9869d32f3176c83f8b43f3
Kyohkotsu <fenrir200791@gmail.com>
2026-01-31 11:05:03 -0500

    commented out test messages in items.rs

--- global_2026/src/item.rs
+++ global_2026/src/item.rs
@@ -1,10 +1,10 @@
 
 pub fn item_setup() {
-    println!("item setup")
+    //println!("item setup")
 }
 pub fn item_update() {
-    println!("item update")
+    //println!("item update")
 }
 pub fn item_fixed_update() {
-    println!("item fixed update")
+    //println!("item fixed update")
 }
