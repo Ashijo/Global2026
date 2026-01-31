68c2858ea3b76fbc0b27a348035ce5cedc3abac8
David Baron <david@nerdtron.com>
2026-01-31 18:30:36 -0500

    First pass implementation of a level entity

--- global_2026/src/main.rs
+++ global_2026/src/main.rs
@@ -7,17 +7,30 @@ mod mask;
 mod blast;
 mod collision;
 mod shared_comp;
+mod level;
 
 use bevy::prelude::*;
 use bevy::camera::ScalingMode;
 use bevy::prelude::OrthographicProjection;
 
+use crate::level::LevelEntity;
+use crate::level::despawn_entities;
+
 const WINDOW_WIDTH:f32 = 1920.0;
 const WINDOW_HEIGHT:f32 = 1080.0;
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash, States, Default)]
+enum GameState {
+    #[default]
+    Playing,
+    GameOver,
+}
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .init_state::<GameState>()
+        .add_systems(OnEnter(GameState::GameOver), restart_game)
         .add_systems(Startup, (main_setup, map::map_setup,enemy::enemy_setup,bomb::bomb_setup, player::player_setup, hud::hud_setup, mask::mask_setup))
         .add_systems(Update, (map::map_update,enemy::enemy_update,bomb::bomb_update, player::player_update, blast::blast_update, hud::hud_update))
         .add_systems(FixedUpdate, (map::map_fixed_update,enemy::enemy_fixed_update,bomb::bomb_fixed_update, player::player_fixed_update, player::player_animation, mask::spawn_masks, blast::blast_collision_system,).chain())
@@ -35,4 +48,9 @@ fn main_setup(mut commands: Commands) {
                        },
                        ..OrthographicProjection::default_2d()
                    }) ));
+}
+
+fn restart_game(mut commands: Commands, entities: Query<Entity, With<LevelEntity>>)
+{
+    despawn_entities(&mut commands, entities.iter());
 }
