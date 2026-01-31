4d3d129d0827c48cd865f66e5ef799c59998f3db
Kyohkotsu <fenrir200791@gmail.com>
2026-01-30 19:45:20 -0500

    player spawn

--- global_2026/src/player.rs
+++ global_2026/src/player.rs
@@ -1,7 +1,16 @@
+use bevy::prelude::*;
 
-
-pub fn player_setup() {
-    println!("player setup")
+pub fn player_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    println!("player setup");
+    commands.spawn((
+        Player,
+        Sprite {
+            custom_size: Some(Vec2::splat(25.)),
+            image: asset_server.load("test.png"),
+            ..default()
+        },
+        Transform::from_xyz(0.0, 0.0, 1.0),
+    ));
 }
 pub fn player_update() {
     println!("player update")
@@ -10,3 +19,6 @@ pub fn player_fixed_update() {
     println!("player fixed update")
 }
 
+#[derive(Component)]
+
+struct Player;
