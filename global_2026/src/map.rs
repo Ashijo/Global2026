07e675e0ec4ed5007d0b5e84f62aee7507189c0e
David Baron <david@nerdtron.com>
2026-01-30 22:44:04 -0500

    Integrating a placeholder map background

--- global_2026/src/map.rs
+++ global_2026/src/map.rs
@@ -1,13 +1,30 @@
 use bevy::prelude::*;
+use crate::WINDOW_WIDTH;
+use crate::WINDOW_HEIGHT;
 
-pub fn map_setup(){
+pub fn map_setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
 
+    commands.spawn((
+        Map,
+        Sprite {
+            custom_size: Some(Vec2::new(WINDOW_WIDTH, WINDOW_HEIGHT)),
+            image: asset_server.load("map.png"),
+            ..default()
+        },
+        Transform::from_xyz(0.0, 0.0, 1.0),
+    ));
 }
 
 pub fn map_update() {
-    println!("map update")
+    //println!("map update")
 }
 
 pub fn map_fixed_update() {
-    println!("map fixed update")
+    //println!("map fixed update")
 }
+
+#[derive(Component)]
+pub struct Map;
