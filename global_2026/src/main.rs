3195d830b7c5a72ceabb7529b6d5f47e3ce0b40b
Kyohkotsu <fenrir200791@gmail.com>
2026-01-31 15:04:17 -0500

    1st version of mask spawner : 15sec to spawn, 3 masks maximum

--- global_2026/src/main.rs
+++ global_2026/src/main.rs
@@ -17,7 +17,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, (main_setup, map::map_setup,enemy::enemy_setup,item::item_setup, player::player_setup, hud::hud_setup, mask::mask_setup))
         .add_systems(Update, (map::map_update,enemy::enemy_update,item::item_update, player::player_update))
-        .add_systems(FixedUpdate, (map::map_fixed_update,enemy::enemy_fixed_update,item::item_fixed_update, player::player_fixed_update, player::player_animation, mask::mask_fixed_upgrade).chain())
+        .add_systems(FixedUpdate, (map::map_fixed_update,enemy::enemy_fixed_update,item::item_fixed_update, player::player_fixed_update, player::player_animation, mask::spawn_masks).chain())
         .run();
 }
 
